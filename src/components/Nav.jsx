7b1917616ce7800928e82cb1833843ae1c6c2d59
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-02-09 21:05:01 -0300

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useCartContext } from 'states/Cart/CartProvider'
+
+import cartIcon from 'assets/cart.png'
+
+const Nav = () => {
+  const { state: { products } } = useCartContext()
+
+  return <nav>
+    <h1>SHOP</h1>
+    <div className='cart-icon-wrapper'>
+      <img src={cartIcon} alt="Cart" />
+      <span>{products.length}</span>
+    </div>
+  </nav>
+}
+
+export default Nav
